557a4f21674fb6d87e5e9570dc4e82447b078874
HieuHoangit <97137357+HieuHoangit@users.noreply.github.com>
2023-09-23 23:05:27 +0700

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const db = mysql.createConnection ({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'socka'
+    database: 'bhuzhqwszdemuzdofxnq'
 });
 
 // connect to database
