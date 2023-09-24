2a858da5b08ef9c134a15728f40b080f45bf6299
HieuHoangit <97137357+HieuHoangit@users.noreply.github.com>
2023-09-24 20:32:06 +0700

    Update home.controller.js

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -10,7 +10,7 @@ exports.getHomePage = (req, res) => {
         }
 
         res.render('index.ejs', {
-            title: "Welcome to Socka | View Players",
+            title: "Fall Shop | View Position",
             players: result
         });
     });
