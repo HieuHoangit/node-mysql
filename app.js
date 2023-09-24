dbce1ff8911bcbd6e097ff67eedc3a960f419aa5
HieuHoangit <97137357+HieuHoangit@users.noreply.github.com>
2023-09-24 16:21:31 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-console.log("Hello");
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const bodyParser = require('body-parser');
@@ -9,14 +8,13 @@ const app = express();
 
 const playerRoutes = require('./routes/player.routes');
 const homeRoutes = require('./routes/index.routes');
-const PORT = process.env.PORT || 2000;
+const PORT = process.env.PORT || = 2000;
 
 
 // create connection to database
 // the mysql.createConnection function takes in a configuration object which contains host, user, password and the database name.
-const mysql      = require('mysql');
 const db = mysql.createConnection ({
-        host     : process.env.MYSQL_ADDON_HOST,
+    host     : process.env.MYSQL_ADDON_HOST,
     	database : process.env.MYSQL_ADDON_DB,
     	user     : process.env.MYSQL_ADDON_USER,
     	password : process.env.MYSQL_ADDON_PASSWORD
@@ -32,6 +30,7 @@ db.connect((err) => {
 global.db = db;
 
 // configure middleware
+app.set('port', process.env.port || port); // set express to use this port
 app.set('views', __dirname + '/views'); // set express to look in this folder to render our view
 app.set('view engine', 'ejs'); // configure template engine
 app.use(bodyParser.urlencoded({ extended: false }));
