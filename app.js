7b4986d7b2c173f393c86f498bc3bb5bb7296fb5
HieuHoangit <97137357+HieuHoangit@users.noreply.github.com>
2023-09-24 16:28:52 +0700

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 2000;
 // create connection to database
 // the mysql.createConnection function takes in a configuration object which contains host, user, password and the database name.
 const db = mysql.createConnection ({
-    host     : process.env.MYSQL_ADDON_HOST,
+        host     : process.env.MYSQL_ADDON_HOST,
     	database : process.env.MYSQL_ADDON_DB,
     	user     : process.env.MYSQL_ADDON_USER,
     	password : process.env.MYSQL_ADDON_PASSWORD
